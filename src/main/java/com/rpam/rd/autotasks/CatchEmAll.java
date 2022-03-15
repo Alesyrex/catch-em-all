bc1638a250ddd70e939b6df206c31266c271991c
Alesyrex <Alesyrex@gmail.com>
2022-03-15 19:39:44 +0300

    add exception handling

--- src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
+++ src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
@@ -1,10 +1,10 @@
 package com.rpam.rd.autotasks;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class CatchEmAll {
 
-    //You may set another exception in this field;
     static Exception exception = new FileNotFoundException();
 
     public static void riskyMethod() throws Exception {
@@ -12,6 +12,15 @@ public class CatchEmAll {
     }
 
     public static void main(String[] args) throws Exception {
-        riskyMethod();
+        try {
+            riskyMethod();
+        } catch (FileNotFoundException e) {
+            throw new IllegalArgumentException("Resource is missing", e);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Resource error", e);
+        } catch (ArithmeticException | NumberFormatException e) {
+            System.err.print(e.getMessage());
+        }
+
     }
 }
